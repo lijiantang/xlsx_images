3d1fc85f1c8ce42e6908ca45f3aedff5465095a3
Alan <alan.qin@letote.cn>
2019-11-08 18:05:22 +0800

    +Windows

---
+++ 下载组件（首次执行）.bat
@@ -0,0 +1,5 @@
+@echo off
+
+echo ----------开始下载必须组件----------
+pip3 install --user -r requirements.txt
+pause
---
+++ 批量下载Excel图片.bat
@@ -0,0 +1,8 @@
+@echo off
+
+echo ----------¿ªÊ¼ÏÂÔØxlsÍ¼Æ¬----------
+cd  /d d:\xlsx_images-master
+python wget_images.py ¿¿¿
+start d:\xlsx_images-master
+echo.
+pause
